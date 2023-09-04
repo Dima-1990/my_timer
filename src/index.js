dbbdda6c7890dd41eedce5bc3500c4f4ab11acb4
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-09-04 21:29:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const btnStartEl = document.querySelector('.btn-start');
 const btnResetEl = document.querySelector('.btn-reset');
 const timetEl = document.querySelector('.global-timer');
 const navigBtnEl = document.querySelector('.global-time-nav');
+const timeListEl = document.querySelector('.time-list');
 
 function pad(value) {
   return String(value).padStart(2, '0');
@@ -12,11 +13,10 @@ function pad(value) {
 const timer = {
   intervalId: null,
   isActive: false,
-
+ 
   start() {
-    if (this.isActive) {
-      return;
-    }
+    if (this.isActive)
+    { return; }
 
     const startTaime = Date.now();
 
@@ -32,7 +32,10 @@ const timer = {
   },
 
   stop() {
+    timetEl.style.backgroundColor="#fff";
     clearInterval(this.intervalId);
+    followUpTime();
+    timetEl.textContent = '00 : 00 : 00';
   },
 
 };
@@ -53,16 +56,53 @@ navigBtnEl.addEventListener('click', e => {
   if (
     !target.classList.contains('btn-start') &
     !target.classList.contains('btn-reset')
-  ) {
-    return;
-  }
+  ) {return;}
 
   if (target.classList.contains('btn-start')) {
+    changeRandomBackgroundColor();
     timer.start();
   }
 
   if (target.classList.contains('btn-reset')) {
-    timer.isActive = false;
     timer.stop();
+    timer.isActive = false;
   }
 });
+
+
+function followUpTime() {
+  if (!timer.isActive) {
+    return;
+  }
+  const paragraphElement = document.createElement('p');
+  paragraphElement.textContent = timetEl.textContent;
+  timeListEl.appendChild(paragraphElement);
+}
+
+
+
+function changeRandomBackgroundColor() {
+  const generateRandomColor = () => {
+    return '#' + (Math.random().toString(16) + '000000').slice(2, 8);
+  };
+
+  const isColorTooDark = color => {
+    const luma =
+      0.299 * parseInt(color.substring(1, 3), 16) +
+      0.587 * parseInt(color.substring(3, 5), 16) +
+      0.114 * parseInt(color.substring(5, 7), 16);
+    return luma < 130; 
+  };
+
+  let randomColor = generateRandomColor();
+
+  while (isColorTooDark(randomColor)) {
+    randomColor = generateRandomColor();
+  }
+
+  timetEl.style.transitionTimingFunction =
+    'cubic-bezier(0.44, 0.43, 0.45, 0.44)';
+  timetEl.style.backgroundColor = randomColor;
+}
+
+
